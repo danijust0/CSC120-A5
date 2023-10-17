59cdf24406cca12d64334c1f87b8502aed3b9ec2
Dani J <djusto@smith.edu>
2023-10-17 19:41:04 -0400

    Java Doc Comments, Reflection and Rubric Done!!

--- Car.java
+++ Car.java
@@ -1,22 +1,32 @@
 import java.util.ArrayList;
 
 public class Car {
+
+    /** Stores passenger list */
     private ArrayList<Passenger> passengersOnboard;
+
+     /** Stores max capacity variable */
     private int maxCapacity;
 
+    /** Constructor for Car */
     public Car (int maxCapacity) {
         this.maxCapacity = maxCapacity;
         this.passengersOnboard = new ArrayList<Passenger>();
     }
 
+    /** Accessor for max capacity of a car */
     public int getCapacity(){
         return this.maxCapacity;
     }
 
+    /** Returns the number of empty seats in a car */
     public int seatsRemaining(){
         return this.maxCapacity - this.passengersOnboard.size();
     }
 
+     /** Adds passenger to car, checks for open seats and if passenger is already on board.
+     * @param Passenger 
+     */
     public void addPassenger(Passenger p){
         if (this.passengersOnboard.contains(p)){
             throw new RuntimeException("Passenger already on board.");
@@ -28,6 +38,9 @@ public class Car {
        
     }
 
+    /** Removes passenger from car, checks if passenger is on board.
+     * @param Passenger 
+     */
     public void removePassenger(Passenger p){
         if (this.passengersOnboard.contains(p) == false){
             throw new RuntimeException("Passenger is not on board.");
@@ -36,6 +49,7 @@ public class Car {
         }
     }
 
+    /** Prints out the names of passengers in a car, checks if car has passengers*/
     public void printManifest(){
         if (this.passengersOnboard.isEmpty()){
             throw new RuntimeException("This car is empty!");
--- Engine.java
+++ Engine.java
@@ -1,23 +1,32 @@
 public class Engine {
+
+    /** Stores fuel variable*/
     private FuelType fuel;
+
+    /** Stores current fuel level */
     double currentFuelLevel;
+
+    /** Stores current max level */
     double maxFuelLevel; 
 
+    /** Constructor for Engine */
     public Engine (FuelType fuel, double maxFuelLevel) {
         this.fuel = fuel;
         this.currentFuelLevel = maxFuelLevel;
         this.maxFuelLevel =  maxFuelLevel;
     }
 
-
+    /** Accessor for FuelType in engine*/
     public FuelType getFuelType(){
         return fuel;
     }
 
+    /** Manipulator that sets current fuel level equal to max fuel level.*/
     public void refuel(){
         this.currentFuelLevel = this.maxFuelLevel;
     }
 
+    /** Decrements fuel level until fuel level is 0 */
     public void go(){
         if (this.currentFuelLevel <= 0) {
             throw new RuntimeException("Fuel level too low! Refuel immediately!");
